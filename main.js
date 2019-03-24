6b54ca0a2a8c1f97b9ea04f4ed0b27c0ed995ba0
shiowwj <shiowweijun@gmail.com>
2019-03-25 01:11:56 +0800

    Commit update: 1. Completed basic logic for block collision 2. To refactor playtwo 3. to add bomb object 4. to add wooden blocks

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// import Game from "/game";
+
 //Wei Jun's version for main js folder
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
@@ -7,42 +9,131 @@ const GAME_HEIGHT = 900;
 const GRID_WIDTH = 100;
 const GRID_HEIGHT = 100;
 
-var img = document.getElementById('playerOneImg');
+var stoneRowCount = 4;
+var stoneColumnCount = 4;
+var stoneWidth = 100;
+var stoneHeight = 100;
+var stonePadding = 100;
+var stoneOffsetTop = 100;
+var stoneOffsetLeft = 100;
 
 
-class blocks {
-    constructor(game_width,game_height){
-        this.width =100;
-        this.height=100;
-
-        this.position = {
-            x : game_width/9,
-            y : game_height/9
-        }
-    }
-
-    draw(ctx){
-        ctx.fillStyle ='#7BB6D6';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+var stones = [];
+for(var c=0; c<stoneColumnCount; c++) {
+    stones[c] = [];
+    for(var r=0; r<stoneRowCount; r++) {
+        stones[c][r] = { x: 0, y: 0, status: 1};
     }
 }
 
-class bomberMan{
-    constructor(game_width,game_height,grid_width,grid_height){
-        this.gameWidth = game_width;
-        this.gameHeight = game_height;
-        this.width = 60;
-        this.height = 60;
-        this.radius = 30;
+class Game {
+    constructor(gameWidth, gameHeight,gridWidth,gridHeight){
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.gridWidth = gridWidth;
+        this.gridHeight = gridHeight;
+    }
+
+    start() {
+        this.board = new Background(this);
+        this.stoneBlock = new Blocks(this);
+        this.playerOne = new BomberMan(this);
+        this.playerTwo = new BomberManTwo(this);
+
+        this.gameObjects = [
+            this.board, this.stoneBlock, this.playerOne, this.playerTwo
+        ];
+
+        new InputHandler(this.playerOne);
+        new InputHandlerTwo(this.playerTwo);
+    }
+
+    update(deltaTime) {
+
+        this.playerOne.update(deltaTime);
+        this.playerTwo.update(deltaTime);
+
+    }
+
+    draw(ctx) {
+
+        this.gameObjects.forEach(object => object.draw(ctx));
+
+    }
+}
+
+class Background {
+    constructor(game){
+        this.width = game.gameWidth;
+        this.height = game.gameHeight;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = '#AB8C68';
+        ctx.fillRect(0, 0, this.width, this.height);
+    } //only background color.
+}
+
+class Blocks {
+    constructor(game){
+
+        // this.top =
+        // this.right =
+        // this.bottom =
+        // this.left =
+        // let topOfBlock = this.game.stoneBlock.position.y;
+        // let rightOfBlock = this.game.stoneBlock.position.x + this.game.stoneBlock.position.gameWidth;
+        // let bottomOfBlock = this.game.stoneBlock.position.y + this.game.stoneBlock.position.gameHeight;
+        // let leftOfBlock = this.game.stoneBlock.position.x;
+
+        this.width = game.gridWidth; // size of block
+        this.height= game.gridHeight;
+
+        this.game = game;
+
+        this.position = { //where to start drawing
+            x : game.gameWidth/9,
+            y : game.gameHeight/9
+        }
+    }
+
+    draw(ctx) {
+    for(var c=0; c<stoneColumnCount; c++) {
+        for(var r=0; r<stoneRowCount; r++) {
+            if(stones[c][r].status == 1){
+            var stoneX = (c*(stoneWidth+stonePadding))+stoneOffsetLeft;
+            var stoneY = (r*(stoneHeight+stonePadding))+stoneOffsetTop;
+            stones[c][r].x = stoneX;
+            stones[c][r].y = stoneY;
+            ctx.beginPath();
+            ctx.rect(stoneX, stoneY, stoneWidth, stoneHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+                }
+            }
+         }
+    } //stone blocks so far
+}
+
+class BomberMan {
+    constructor(game){
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+        this.gridWidth = game.gridWidth;
+        this.gridHeight = game.gridHeight;
+        this.radius = 25;
 
         this.maxSpeedX = 9;
         this.maxSpeedY = 9;
         this.speedX = 0;
         this.speedY = 0;
 
+        this.game = game;
+
         this.position = {
-            x : grid_width/2,
-            y : grid_height + grid_height/2
+            x : this.gridWidth/2,
+            y : this.gridHeight/2
         }
     }
 
@@ -71,30 +162,173 @@ class bomberMan{
 
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+        ctx.linewidth = 3;
+        ctx.strokeStyle="red";
+        ctx.fillStyle = "green";
+        ctx.fill();
         ctx.stroke();
-        // ctx.fillStyle = '#CB8CF9;
     }
 
     update(deltaTime) {
-        if(!deltaTime) return;
+            // console.log(this.game.playerOne.position.x);
             this.position.x += this.speedX;
             this.position.y += this.speedY;
 
-        if(this.position.x < this.radius)
+    //checks if bomberman on left or right wall
+        if(this.position.x < this.radius){
             this.position.x =this.radius;
+        }
 
-        if(this.gameWidth - this.radius < this.position.x)
+        if(this.gameWidth - this.radius < this.position.x){
             this.position.x = this.gameWidth - this.radius;
+        }
 
-        if(this.position.y < this.radius)
+    //checks if bomberman on top and bottom wall.
+        if(this.position.y < this.radius){
             this.position.y = this.radius;
+        }
 
-        if(this.gameHeight - this.radius < this.position.y)
+        if(this.gameHeight - this.radius < this.position.y){
             this.position.y = this.gameHeight - this.radius;
+        }
+
+    //checks if bomberman on stone blocks (blocks) //40min on video
+        let player_XpositionRight = this.position.x + this.radius;
+        let player_XpositionLeft = this.position.x - this.radius;
+        let player_YpositionTop = this.position.y - this.radius;
+        let player_YpositionBottom = this.position.y + this.radius
+
+        for(var c=0; c<stoneColumnCount; c++) {
+            for(var r=0; r<stoneRowCount; r++) {
+                var b = stones[c][r];
+                if(b.status == 1) {
+                    if(player_XpositionRight > b.x
+                        && player_XpositionLeft < b.x+stoneWidth
+                        && player_YpositionBottom > b.y
+                        && player_YpositionTop < b.y+stoneHeight){
+                        if(this.speedX > 0){
+                            this.position.x = b.x - this.radius;
+                        } else if(this.speedX<0){
+                            this.position.x = b.x + stoneWidth + this.radius;
+                        } else if(this.speedY>0){
+                            this.position.y = b.y - this.radius;
+                        } else if(this.speedY<0){
+                            this.position.y = b.y +stoneHeight + this.radius;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}// circle representing bomber-man
+
+class BomberManTwo {
+    constructor(game){
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+        this.gridWidth = game.gridWidth;
+        this.gridHeight = game.gridHeight;
+        this.radius = 25;
+
+        this.maxSpeedX = 9;
+        this.maxSpeedY = 9;
+        this.speedX = 0;
+        this.speedY = 0;
+
+        this.game = game;
+
+        this.position = {
+            x : this.gameWidth - this.gridWidth/2,
+            y : this.gameHeight - this.gridHeight/2
+        }
+    }
+
+    moveLeft(){
+        this.speedX = -this.maxSpeedX;
+    }
+
+    moveRight(){
+        this.speedX = this.maxSpeedX;
+    }
+
+    moveUp(){
+        this.speedY = -this.maxSpeedY;
+    }
+
+    moveDown(){
+        this.speedY = this.maxSpeedY;
+    }
+
+    stop(){
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+
+    draw(ctx) {
+
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+        ctx.linewidth = 3;
+        ctx.strokeStyle="green";
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    update(deltaTime) {
+            // console.log(this.game.playerOne.position.x);
+            this.position.x += this.speedX;
+            this.position.y += this.speedY;
+
+    //checks if bomberman on left or right wall
+        if(this.position.x < this.radius){
+            this.position.x =this.radius;
+        }
+
+        if(this.gameWidth - this.radius < this.position.x){
+            this.position.x = this.gameWidth - this.radius;
+        }
+
+    //checks if bomberman on top and bottom wall.
+        if(this.position.y < this.radius){
+            this.position.y = this.radius;
+        }
+
+        if(this.gameHeight - this.radius < this.position.y){
+            this.position.y = this.gameHeight - this.radius;
+        }
+
+    //checks if bomberman on stone blocks (blocks) //40min on video
+        let player_XpositionRight = this.position.x + this.radius;
+        let player_XpositionLeft = this.position.x - this.radius;
+        let player_YpositionTop = this.position.y - this.radius;
+        let player_YpositionBottom = this.position.y + this.radius
+
+        for(var c=0; c<stoneColumnCount; c++) {
+            for(var r=0; r<stoneRowCount; r++) {
+                var b = stones[c][r];
+                if(b.status == 1) {
+                    if(player_XpositionRight > b.x
+                        && player_XpositionLeft < b.x+stoneWidth
+                        && player_YpositionBottom > b.y
+                        && player_YpositionTop < b.y+stoneHeight){
+                        if(this.speedX > 0){
+                            this.position.x = b.x - this.radius;
+                        } else if(this.speedX<0){
+                            this.position.x = b.x + stoneWidth + this.radius;
+                        } else if(this.speedY>0){
+                            this.position.y = b.y - this.radius;
+                        } else if(this.speedY<0){
+                            this.position.y = b.y +stoneHeight + this.radius;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-class inputHandler{
+class InputHandler {
     constructor(playerOne) {
         document.addEventListener('keydown',event => {
             switch(event.keyCode) {
@@ -142,74 +376,68 @@ class inputHandler{
     }
 }
 
+class InputHandlerTwo {
+    constructor(playerTwo) {
+        document.addEventListener('keydown',event => {
+            switch(event.keyCode) {
+                case 76:
+                    playerTwo.moveLeft();
+                    break;
 
-let squareBlock = new blocks(GAME_WIDTH,GAME_HEIGHT);
-let playerOne = new bomberMan(GAME_WIDTH,GAME_HEIGHT,GRID_WIDTH,GRID_HEIGHT);
+                case 222:
+                    playerTwo.moveRight();
+                    break;
 
-new inputHandler(playerOne);
+                case 80:
+                    playerTwo.moveUp();
+                    break;
 
-squareBlock.draw(ctx);
-playerOne.draw(ctx);
-ctx.drawImage(img,0,0,100,100);
+                case 186:
+                    playerTwo.moveDown();
+                    break;
 
+            }
+        });
+        document.addEventListener('keyup',event => {
+            switch(event.keyCode) {
+                case 76:
+                    if(playerTwo.speedX < 0 )
+                    playerTwo.stop();
+                    break;
 
+                case 222:
+                    if(playerTwo.speedX >0)
+                    playerTwo.stop();
+                    break;
+
+                case 80:
+                    if(playerTwo.speedY < 0)
+                    playerTwo.stop();
+                    break;
+
+                case 186:
+                    if(playerTwo.speedY > 0)
+                    playerTwo.stop();
+                    break;
+            }
+        });
+    }
+}
+
+let game = new Game(GAME_WIDTH,GAME_HEIGHT,GRID_WIDTH,GRID_HEIGHT);
+game.start();
 
 let lastTime = 0;
-
-function updateGameBoard(timestamp){
+function updateGameBoard(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    playerOne.update(deltaTime);
-    playerOne.draw(ctx);
-    squareBlock.draw(ctx);
-    ctx.drawImage(img,0,0,100,100);
+
+    game.update(deltaTime);
+    game.draw(ctx);
 
     requestAnimationFrame(updateGameBoard);
 }
 
-updateGameBoard();
-/*
-var players = new Array();
-
-var objects = new Array();
-
-var timer = null;
-
-var game_started = false;
-
-var num_blocks = null;
-
-var num_blocks_left = null;
-
-intro_screen = function() {
-    var center;
-    center = $('#map').width()/2;
-    return $('#map').drawText({
-        fillStyle: '#000',
-        x: center,
-        y: 100,
-        text: 'Bomber Man',
-        font: '50pt Helvetica, serif'
-    }).drawText({
-        fillstyle: '#000',
-        x: center,
-        y: 300,
-        text: 'Press XXX to start',
-        font: '20pt Helvetica, serif'
-    });
-};
-//use constructor to create canvas blocks  (stone, empty, wood )
-
-
-// Set 2d array. Predetermined spaces that are empty(start location for player). Set values of array to be stone. Randomize the rest of the array to be either empty spaces or wooden blocks
-
-//using the array with set values eariler, use canvas to generate pixels for the objects.
-
-
-$(document).ready(function() {
-  return intro_screen();
-});
-
-*/
+requestAnimationFrame(updateGameBoard);
